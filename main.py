d277488997dad367310c94b78e774bf371d79524
Пользователь <Erkhan>
2024-11-25 22:38:34 +0600

    I commited 3 changes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-with open("/Users/user/Desktop/my_text.txt") as file:
+with open("my_text.txt") as file:
     read = file.read()
     print(read)
 
 print('Hello World!')
+
+print('CHANGE0')
+
+
+for i in range(2, 8):
+    print(i)
